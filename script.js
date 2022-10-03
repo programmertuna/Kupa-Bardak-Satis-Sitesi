b0929b97068b3c6d3759aee504c2895e44ae475a
Tunahan Baydar <94986928+programmertuna@users.noreply.github.com>
2022-10-03 20:40:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+   /* Site Linkleri*/
+
+document.querySelector("#insta").onclick=function()
+  {
+    window.location.href="https://www.instagram.com/kupastorem/"
+  }
+  
+document.querySelector("#footinsta").onclick=function()
+{
+  window.location.href="https://www.instagram.com/kupastorem/"
+}
+
+  
+  document.querySelector("#face").onclick=function()
+  {
+    window.location.href="https://www.facebook.com/Tunahan.baydar2/"
+  }
+
+ 
+  document.querySelector("#magaza").onclick=function()
+  {
+    window.location.href=" https://shopier.com/kupastorem"
+  }
+  document.querySelector("#store").onclick=function()
+  {
+    window.location.href=" https://shopier.com/kupastorem"
+  }
+  document.querySelector("#shopier").onclick=function()
+  {
+    window.location.href=" https://shopier.com/kupastorem"
+  }
+
+     /* örnek ürün Linkleri*/
+
+
+  document.querySelector("#berlin").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13256964"
+  }
+
+  document.querySelector("#fight").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13257122"
+  }
+  document.querySelector("#aile").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13257536"
+  }
+  document.querySelector("#lord").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13192300"
+  }
+  document.querySelector("#yüzük").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13192300"
+  }
+  document.querySelector("#logo").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13188598"
+  }
+  document.querySelector("#anime").onclick=function()
+  {
+    window.location.href=" https://www.shopier.com/ShowProductNew/products.php?id=13188554"
+  }
+  document.querySelector("#valo").onclick=function()
+  {
+    window.location.href="https://www.shopier.com/ShowProductNew/products.php?id=13188582"
+  }
+
+
+ 
